3507ef722f04860626a8b4a20d680facf7a44be7
mooncn <qkmyyx@163.com>
2020-08-15 21:00:24 +0800

    renew

--- config.php
+++ config.php
@@ -18,11 +18,11 @@ $key = "mooncn.win";
 
 //SMTP Server Information
 $mail_deliver = true; //Default Enable The Mail Deliver data function.
-$smtp_host = "";// Set the SMTP server to send through
+$smtp_host = "smtp.sample.com";// Set the SMTP server to send through
 $smtp_auth = true;//true or false Enable SMTP authentication
-$smtp_username = "";// SMTP username
-$smtp_password = "";// SMTP password
-$smtp_port = "";// TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+$smtp_username = "username@sample.com";// SMTP username
+$smtp_password = "password";// SMTP password
+$smtp_port = "465";// TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 //max server space backup files nums.
-$max_backup = 3;
+$max_backup = 0;

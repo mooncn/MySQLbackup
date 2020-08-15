6a8dc9e17ddbe5cfe7ca9c2ace868945d7d83470
mooncn <qkmyyx@163.com>
2020-08-15 18:10:21 +0800

    split config info
    Configuration Information File

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+* Created By PhpStorm
+* Author : MOONCN
+* Date : 2020/8/15
+* Time : 15:04:29
+* Blog : http://mooncn.win
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+//DB information
+$db_host = "localhost";
+$db_user_name = "root";
+$db_user_pass = "root";
+$db_name = "test";
+
+//access key
+$key = "mooncn.win";
+
+//SMTP Server Information
+$mail_deliver = true; //Default Enable The Mail Deliver data function.
+$smtp_host = "";// Set the SMTP server to send through
+$smtp_auth = true;//true or false Enable SMTP authentication
+$smtp_username = "";// SMTP username
+$smtp_password = "";// SMTP password
+$smtp_port = "";// TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+//max server space backup files nums.
+$max_backup = 3;
